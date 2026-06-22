8678ec9c382e24be9da473ece6edb3fd3b8ac17e
abdo-hany <baltmsah8@gmail.com>
2026-06-22 22:38:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// design of ul li in active
+
+let lis = document.querySelectorAll('ul li');
+
+lis.forEach(li => {
+  li.onclick = e => {
+    if (e.target.tagName === 'A') {
+      document.querySelector('ul li.active')?.classList.remove('active');
+      li.target.classList.add('active');
+     };
+  };
+});
+
+// button of up page
+
+let btnUp = document.querySelector('.up');
+
+window.onscroll = () => {
+  if (window.scrollY >= 200) {
+    btnUp.classList.add('show');
+  } else {
+    btnUp.classList.remove('show');
+  };
+};
+
+btnUp.onclick = () => {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+};
